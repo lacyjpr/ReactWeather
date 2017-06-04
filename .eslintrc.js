19260a031a7db02102c8388ff8de2239144fc392
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-06-03 21:17:02 -0700

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,22 @@
 module.exports = {
     "env": {
         "browser": true,
+        "commonjs": true,
         "es6": true,
         "node": true
     },
     "extends": "eslint:recommended",
-    "installedESLint": true,
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
             "jsx": true
-        }
-    },
-    "globals": {
-        "React": true
+        },
+        "sourceType": "module"
     },
     "plugins": [
         "react"
     ],
     "rules": {
-        "no-console":0,
         "indent": [
             "error",
             "tab"
