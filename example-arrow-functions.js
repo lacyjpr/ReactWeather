8b248628d0a1979e2be5bcee1bb5110e856f4d03
Jean-Paul Lacy <lacyjpr@gmail.com>
2017-06-25 10:15:40 -0700

    Add api functionality

---
+++ example-arrow-functions.js
@@ -0,0 +1,43 @@
+var names = ['Andrew', 'Julie', 'Jen'];
+
+// names.forEach(function (name){
+// 	console.log('forEach', name);
+// });
+
+// names.forEach((name) => {
+// 	console.log('arrowFunc', name);
+// });
+
+// names.forEach((name) => console.log(name));
+
+// var returnMe = (name) => name + '!';
+// console.log(returnMe('JP'));
+
+// var person = {
+// 	name: 'JP',
+// 	greet: function () {
+// 		names.forEach((name) => {
+// 			console.log(this.name + ' says hi to ' + name);
+// 		});
+// 	}
+// };
+
+// person.greet();
+
+function add (a, b) {
+	return a + b;
+}
+
+// addStatement
+var addStatement = (a, b) => {
+	return a + b;
+}
+console.log(addStatement(1, 3));
+console.log(addStatement(9, 0));
+// addExpression
+var addExpression = (a, b) => a + b;
+console.log(addExpression(1, 3));
+console.log(addExpression(9, 0));
+
+// console.log(add(1, 3));
+// console.log(add(9, 0));
